db8d3a04b4646b72723d11663320b0f8e448c968
xin <791950426@qq.com>
2021-03-15 11:37:36 +0800

    u

--- apps/xmu-sishi-server/www.py
+++ apps/xmu-sishi-server/www.py
@@ -20,17 +20,17 @@ from webs.controllers.index import route_index
 from webs.controllers.Track import route_track
 from webs.controllers.Trace import route_trace
 from webs.controllers.Business import route_business
-from webs.controllers.API import route_api
+# from webs.controllers.API import route_api
 
 app.register_blueprint(route_wechat, url_prefix="/wechat")
-app.register_blueprint(route_admin, url_prefix="/admin")
+app.register_blueprint(route_admin, url_prefix="/api/admin")
 app.register_blueprint(route_user, url_prefix="/user")
 app.register_blueprint(route_venue, url_prefix="/venue")
 app.register_blueprint(route_index, url_prefix="/")
 app.register_blueprint(route_track, url_prefix="/track")
 app.register_blueprint(route_trace, url_prefix="/trace")
 app.register_blueprint(route_business, url_prefix="/business")
-app.register_blueprint(route_api, url_prefix="/api")
+# app.register_blueprint(route_api, url_prefix="/api")
 
 
 
