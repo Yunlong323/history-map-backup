0e29de731f3709c80e52244bbcfdd9ab9a9622fb
Yunlong LIU <1035268937@qq.com>
2021-04-28 20:05:33 +0800

    master

---
+++ learngit.cpp
@@ -0,0 +1,12 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout<<"拿起来git，学习如何用vscode工作git然后pull，push，merge\n";
+    cout<<"学会java\n";
+    cout<<"今晚写完计网立马交，然后回宿舍吃饭，看计组，不能再拖了\n";
+    cout<<"明天中午上午看sql\n";
+    return 0;
+}
